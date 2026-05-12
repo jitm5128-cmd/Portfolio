16e06bbc297947e28563c8df9a2a60f31a48f285
jitm5128-cmd <jitm5128@gmail.com>
2026-05-12 20:10:45 +0530

    updated portfolio website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,31 @@
 /*=============== HOME SPLIT TEXT ===============*/
+const { animate, splitText, stagger} = anime;
+const { chars: chars1 } = splitText('.home__profession-1', {chars: true})
+const { chars: chars2 } = splitText('.home__profession-2', {chars: true})
 
+animate(chars1, {
+    y: [
+      { to: ['100%', '0%'] },
+      { to: '-100%', delay: 3000, ease: 'in(3)' }
+    ],
+    duration: 900,
+    ease: 'out(3)',
+    delay: stagger(80),
+    loop: true,
+  })
 
+animate(chars2, {
+    y: [
+      { to: ['100%', '0%'] },
+      { to: '-100%', delay: 3000, ease: 'in(3)' }
+    ],
+    duration: 900,
+    ease: 'out(3)',
+    delay: stagger(80),
+    loop: true,
+  })
+
+  
 /*=============== SWIPER PROJECTS ===============*/
 
 
