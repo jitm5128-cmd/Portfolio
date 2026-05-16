d2ed25c6c51a56038dfdd7dc1f6116080366e9af
jitm5128-cmd <jitm5128@gmail.com>
2026-05-16 23:03:58 +0530

    update portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,21 @@ animate(chars2, {
   
 /*=============== SWIPER PROJECTS ===============*/
 
-
+const swiperProjects = new Swiper('.projects__swiper', {
+  loop: true,
+  spaceBetween: 24,
+  slidesPerView: 'auto',
+  grabCursor: true,
+  speed: 600,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay:{
+    delay:3000,
+    disableOnInteraction: false,
+  }
+})
 /*=============== WORK TABS ===============*/
 
 
